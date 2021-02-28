3526dc146ea856cb6a776dbc3390bf4811c2c696
eduardonavarrohernandez <enavarro1@outlook.es>
2021-02-28 03:08:20 +0000

    segunda carga

--- test.js
+++ test.js
@@ -10,8 +10,8 @@ test("One euro should be 1.206 dollars", () => {
     const dollars = fromEuroToDollar(3.5)
 
     // is 1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
-    const expected = 3.5 * 1.2; 
-    
+    const expected = 3.5 * 1.2;
+
     // hago mi comparacion (la prueba)
     expect(expected).toBe(dollars);
 })
@@ -26,8 +26,16 @@ test("One euro should be 1.206 dollars", () => {
     const cantidadYenes = fromDollarToYen(3.5)
 
     // is 1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
-    const expected = (3.5/1.2)*127.9; 
-    
+    const expected = (3.5 / 1.2) * 127.9;
+
     // hago mi comparacion (la prueba)
     expect(expected).toBe(cantidadYenes);
 })
+
+
+test("One yen should be 1.206 dollars", () => {
+    const { fromYenToPound } = require('./app.js');
+    const yen = fromYenToPound(1);
+    const expected = (1 / 127.9) * 0.8;
+    expect(expected).toBe(yen);
+});
